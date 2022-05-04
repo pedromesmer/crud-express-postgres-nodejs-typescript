3f7c038a8b761516c2238585a634aa1fdfe26f89
pedromesmer <pedro_mesmer@hotmail.com>
2022-05-04 11:33:12 -0300

    controller add

---
+++ src/modules/users/infra/http/controllers/UsersController.ts
@@ -0,0 +1,41 @@
+import { CreateUserService } from '@modules/users/services/CreateUserService';
+import { RemoveUserService } from '@modules/users/services/RemoveUserService';
+import { ShowAllUsersService } from '@modules/users/services/ShowAllUsersService';
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+class UsersController {
+  public async create(request: Request, response: Response) {
+    const { name } = request.body;
+
+    const createUserService = container.resolve(CreateUserService);
+
+    const user = await createUserService.execute({
+      name,
+    });
+
+    return response.json(user);
+  }
+
+  public async show(request: Request, response: Response) {
+    const showUsersService = container.resolve(ShowAllUsersService);
+
+    const users = await showUsersService.execute();
+
+    return response.json(users);
+  }
+
+  public async remove(request: Request, response: Response) {
+    const { id } = request.params;
+
+    const removeUserService = container.resolve(RemoveUserService);
+
+    await removeUserService.execute({
+      id,
+    });
+
+    return response.send();
+  }
+}
+
+export { UsersController };
